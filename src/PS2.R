d9640ef3e671f9037a79801924e354037d95895b
LCruzFer <lucas.cruz@hotmail.de>
2021-04-22 14:25:18 +0200

    knn now working

--- src/PS2.R
+++ src/PS2.R
@@ -12,17 +12,18 @@ knn_prediction = function(x, y, k, x0){
   #need a distance matrix, i.e. for each point in x0 have a column with distances to points in x (training set)
   #then rank the values in each column and get the ones from 1 to k
   #use the respective observations in the y vector to calculate the mean over them as the prediction 
-  #set up empty distance matrix
-  distmat = matrix(NA, nrow = length(x), ncol = length(x))
+  #set up empty distance matrix, nrows must be length of our training set x 
+  #(element i,j shows distance of training point i to test point j)
+  distmat = matrix(NA, nrow = length(x), ncol = length(x0))
   #set up empty predictions vector
   predictions = rep(NA, length(x0))
   #loop over each point in test sample
   for (i in 1:length(x0)){
     #get distances to points in training sample
     distmat[, i] = euclid_dist(x0[i], x)
-    #get rank of distances 
-    ranksmat = apply(distmat, 2, rank, ties.method = "random")
   }
+  #get rank of distances 
+  ranksmat = apply(distmat, 2, rank, ties.method = "random")
   #now get the values with rank 1 to k in each column 
   for (i in 1:length(x0)){
     #get a vector containing true/false depending on whether point is one of k nearest neigbors (rank btw 1 and k)
@@ -34,7 +35,6 @@ knn_prediction = function(x, y, k, x0){
   }
   return(predictions)
 }
-
 
 x0 = rnorm(100, 2, 10)
 x = rnorm(100, 2, 10)
@@ -50,6 +50,7 @@ dim(flights)
 #a) only keep the data for which arr_delay and dep_delay are not NA 
 flights = flights[is.na(flights$arr_delay) == FALSE, ]
 flights = flights[is.na(flights$dep_delay) == FALSE, ]
+
 #b) 
 x0 = seq(-10, 50, length.out = 100)
 y = flights$arr_delay 
@@ -58,4 +59,15 @@ x = flights$dep_delay
 predictions = seq(1, length(x0))
 for (k in c(1, 20, 200)){
   predictions = cbind(predictions, knn_prediction(x, y, k, x0))
+  print(k)
 }
+predictions = predictions[, -1]
+#bind together in dataframe 
+pred_df = data.frame(cbind(x0, predictions))
+names(pred_df) = c('x0', 'k1', 'k20', 'k200')
+#c) 
+plot = ggplot(pred_df, aes(x=x0))
+plot + geom_line(aes(y=k1, color='darkred')) +
+       geom_line(aes(y=k20, color='blue')) +
+       geom_line(aes(y=k200, color='green')) + 
+       scale_color_manual(labels=c('k=1', 'k=20', 'k=200'))
